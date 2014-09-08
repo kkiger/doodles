92311335b4a2aa648b8983578d14fb184e0459d8
Kali Kiger <kalikiger@gmail.com>
2014-09-07 23:07:39 -0700

    added question and progress to readme and added treehouse java notes as new file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Hello, world
+Hello, World.
+
+Question - what do programmers tweet about?
+
+Things I did towards my goals this weekend:
+Listened to 5 Minutes of Java podcast
+Listened to Google Talk about Java: Only the Good Things
---
+++ treehouse_java_notes.js
@@ -0,0 +1,13 @@
+Treehouse Javascript Notes
+
+var my_array = [2, 3, 4];
+console.log(my_array.toString());
+
+//adds 5 to end of array
+my_array.push(5)
+console.log(my_array.toString());
+
+.toString() makes it so if you have a console.log soon
+after printing the array, it will print the array properly
+with only the 3 exisiting values versus printing the first
+log and second log the same
